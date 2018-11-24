a67c6d539726ef7677137f11bcde3f9bb931819b
YukiMOB <yuki.rx0126@gmail.com>
2018-11-24 16:20:25 +0900

    速度平均のリスト化

--- velocityu.R
+++ velocityu.R
@@ -1,12 +1,16 @@
 # 平均移動速度の導出
-velocity_average <- function(df,fl){
-  for (i in 1:length(fl)) {
+velocity_average <- function(df,start,end){
+  list.velocity_average <- c()
+  for (i in start:end) {
     x <- subset(df$x,df$id == i)
     y <- subset(df$y,df$id == i)
     average.velocity <- mean(abs(diff(x)))
-    print(average.velocity)
+    list.velocity_average <- c(list.velocity_average,average.velocity)
   }
+  return(list.velocity_average)
 }
 
 # 統計解析
-velocity_average(df.downsamp,fl)
+it10.list.velocity_average <- velocity_average(df.downsamp,1,5)
+it40.list.velocity_average <- velocity_average(df.downsamp,6,10)
+it160.list.velocity_average <- velocity_average(df.downsamp,11,15)
