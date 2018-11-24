7ca1b4467796bf4df1e498aeccb012b53363fe21
YukiMOB <yuki.rx0126@gmail.com>
2018-11-24 16:01:34 +0900

    距離データのリスト化

--- distancw.R
+++ distancw.R
@@ -1,13 +1,17 @@
 # 総移動距離の導出
-residence_time <- function(df,fl){
-  for (i in 1:length(fl)) {
+distance <- function(df,start,end){
+  list.distance <- c()
+  for (i in start:end) {
     x <- subset(df$x,df$id == i)
     y <- subset(df$y,df$id == i)
     distance <- sum(sqrt(diff(x)^2 + diff(y)^2))
-    print(distance)
+    # print(distance)
+    list.distance <- c(list.distance,distance)
   }
+  return(list.distance)
 }
-
 # 統計解析
-
-residence_time(df.downsamp,fl)
+# 5試行ごとなので
+it10.residence.list <- distance(df.downsamp,1,5)
+it14.residence.list <- distance(df.downsamp,6,10)
+it160.residence.list <- distance(df.downsamp,11,15)
