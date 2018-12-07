1df8b1d425ef184aa2b2076797eb0a5a1aa90c37
YukiMOB <yuki.rx0126@gmail.com>
2018-12-07 10:41:04 +0900

    試行毎のリスト化を簡略化

--- velocityu.R
+++ velocityu.R
@@ -12,11 +12,11 @@ velocity_average <- function(dt,start,end){
 }
 
 # 統計解析
-it10.list.velocity_average <- velocity_average(dt,1,5)
-it40.list.velocity_average <- velocity_average(dt,6,10)
-it160.list.velocity_average <- velocity_average(dt,11,15)
-staticLeft.list.velocity_average <- velocity_average(dt,16,20)
-staticRight.list.velocity_average <- velocity_average(dt,21,25)
+it10.list.velocity_average <- velocity_average(dt,exNumlist[1],exNumlist[2])
+it40.list.velocity_average <- velocity_average(dt,exNumlist[2] + 1,exNumlist[3])
+it160.list.velocity_average <- velocity_average(dt,exNumlist[3] + 1,exNumlist[4])
+staticLeft.list.velocity_average <- velocity_average(dt,exNumlist[4] + 1,exNumlist[5])
+staticRight.list.velocity_average <- velocity_average(dt,exNumlist[5] + 1,exNumlist[6])
 NoStimulus.list.Velocity_average <- velocity_average(dt,1,15)
 
 # 正規分布かどうかの確認用
